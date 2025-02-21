20a3fd0311a901dc09da32012bd078986a7ba3f0
Neel Smith <dnsmith.neel@gmail.com>
2025-02-21 18:37:23 -0500

    Build private site to Dropbox

--- sandbox/publish.jl
+++ sandbox/publish.jl
@@ -14,5 +14,6 @@ destdir = "/Users/nsmith/Desktop/famhist/quart-personal/familyhistory/sources"
 # Publish public:
 exportvault(gv, destdir)
 
-privatedest = joinpath(pwd(), "scratch", "privateexport")
+#privatedest = joinpath(pwd(), "scratch", "privateexport")
+privatedest = "/Users/nsmith/Dropbox/_current_projects/_genealogy/_privateexport"
 exportvault(gv, privatedest; publiconly = false)
