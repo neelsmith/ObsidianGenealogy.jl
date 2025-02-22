ad6a9a536c4b5268ccab8279f395b1316e01c09b
Neel Smith <dnsmith.neel@gmail.com>
2025-02-22 05:11:04 -0500

    Simplify script

--- sandbox/publish.jl
+++ sandbox/publish.jl
@@ -4,16 +4,10 @@ using Markdown
 vaultdir = "/Users/nsmith/Dropbox/_obsidian/family-history"
 gv = Vault(vaultdir) |> genealogyVault
 
-# Choose one:
-# Mac mini:
-destdir = "/Users/nsmith/Desktop/famhist/quart-personal/familyhistory/sources"
-# Laptop:
-#destdir = "/Users/nsmith/Desktop/family-site/quart-personal/familyhistory/sources"
-
-
 # Publish public:
+destdir = "/Users/nsmith/Desktop/family-site/quart-personal/familyhistory/sources"
 exportvault(gv, destdir)
 
-#privatedest = joinpath(pwd(), "scratch", "privateexport")
+# Publish private:
 privatedest = "/Users/nsmith/Dropbox/_current_projects/_genealogy/_privateexport"
 exportvault(gv, privatedest; publiconly = false)
